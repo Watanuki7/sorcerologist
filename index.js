f90d431497800b90cda38a798a7eab91ae513fe5
Watanuki <sorcerologist7@gmail.com>
2024-11-08 21:45:32 -0300

    Fix toggle

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggleButton = document.getElementById('mobile-menu-toggle');
+    const menuToggleButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
     const menu = document.getElementById('navbar-default');
 
+    // Check if both elements exist before adding the event listener
     if (menuToggleButton && menu) {
         menuToggleButton.addEventListener('click', () => {
+            // Toggle the 'hidden' class to show or hide the mobile menu
             menu.classList.toggle('hidden');
         });
     }
