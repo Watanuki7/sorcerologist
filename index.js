c6679dcc70771dcfd5c46e7f65f6b1c8888e907e
Watanuki <sorcerologist7@gmail.com>
2024-11-08 21:41:59 -0300

    Fix listener

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const menuToggleButton = document.getElementById('mobile-menu-toggle');
-const menu = document.getElementById('navbar-default');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggleButton = document.getElementById('mobile-menu-toggle');
+    const menu = document.getElementById('navbar-default');
 
-menuToggleButton.addEventListener('click', () => {
-    // Toggle the 'hidden' class to show or hide the mobile menu
-    menu.classList.toggle('hidden');
+    if (menuToggleButton && menu) {
+        menuToggleButton.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+    }
 });
